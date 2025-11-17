fe94ca16a076b19a7b79306d43a546fc44df0d3f
aamirwebteam <aamir@webteampl.com>
2025-11-17 17:54:25 +0530

    Initial commit: Node.js Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://aamirejazahmed_db_user:qpAoZXw0tsdvXpXf@rsto.iodiwqh.mongodb.net/?appName=RSTO', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
